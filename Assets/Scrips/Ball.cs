a66f689f1e2e9348885f7fb71e875678944e2a4d
KumarSwaraj <swarajswain00@gmail.com>
2020-11-21 18:40:50 +0530

    2D

---
+++ Assets/Scrips/Ball.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private Vector3 paddleToBallVector;
+	private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle> ();
+		paddleToBallVector = this.transform.position - paddle.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			LaunchBallAtStart ();
+		}
+	}
+
+	void LaunchBallAtStart() {
+		// Lock the ball relative to the paddle at the start
+		this.transform.position = paddle.transform.position + paddleToBallVector;
+
+		// Wait until the left button of the mouse is pressed
+		if (Input.GetMouseButtonDown (0)) {
+			this.GetComponent<Rigidbody2D> ().velocity = new Vector2 (2f, 10f);
+
+			hasStarted = true;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		ChangeVelocity ();
+		PlayBounceSound (col);
+	}
+
+	void ChangeVelocity () {
+		Vector2 tweak = new Vector2 (Random.Range(0f,0.2f), Random.Range(0,0.2f));
+
+		this.GetComponent<Rigidbody2D> ().velocity += tweak;
+	}
+
+	void PlayBounceSound (Collision2D col) {
+		if (hasStarted && (col.gameObject.name.Equals("Padle")) || (col.gameObject.name.Equals("Wall_Left"))||
+			(col.gameObject.name.Equals("Wall_Right"))|| (col.gameObject.name.Equals("Wall_Top"))) {
+			AudioSource audio = GetComponent<AudioSource> ();
+			audio.Play ();
+		}
+	}
+}
---
+++ Assets/Scrips/Paddle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public bool autoPlay = false;
+
+	private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.FindObjectOfType<Ball> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (autoPlay) {
+			AutoMove ();
+		} else {
+			MoveWithMouse ();
+		}
+	}
+
+	void MoveWithMouse () {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		float mousePositionInBlocks = ((Input.mousePosition.x / Screen.width) * 16) - 8;
+
+		paddlePos.x = Mathf.Clamp (mousePositionInBlocks, -7.5f, 7.5f);
+		this.transform.position = paddlePos;
+	}
+
+	void AutoMove() {
+		Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+		Vector3 ballPosition = ball.transform.position;
+
+		paddlePos.x = Mathf.Clamp (ballPosition.x, -7.5f, 7.5f);
+		this.transform.position = paddlePos;
+	}
+}
